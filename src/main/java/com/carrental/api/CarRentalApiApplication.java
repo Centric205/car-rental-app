10dc216eaa8006efa068b210cc11f735e9b9e6ed
Theo Madikgetla <theothedeveloper@gmail.com>
2025-12-16 05:26:11 +0200

    BCRA-8 Renamed carental package to carrental

--- src/main/java/com/carrental/api/CarRentalApiApplication.java
+++ src/main/java/com/carrental/api/CarRentalApiApplication.java
@@ -1,4 +1,4 @@
-package com.carental.api;
+package com.carrental.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/carrental/api/CarRentalApiApplicationTests.java
+++ src/test/java/com/carrental/api/CarRentalApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.carental.api;
+package com.carrental.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
