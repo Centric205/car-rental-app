e4634cdc791d50500b4c5f765518ff1ad5425af6
Theo Madikgetla <theothedeveloper@gmail.com>
2025-12-17 00:18:06 +0200

    Added logging visibility for displaying profiles in logs

--- src/main/java/com/carrental/api/CarRentalApiApplication.java
+++ src/main/java/com/carrental/api/CarRentalApiApplication.java
@@ -1,13 +1,30 @@
 package com.carrental.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class CarRentalApiApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(CarRentalApiApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(CarRentalApiApplication.class, args);
+
+		ConfigurableApplicationContext context = SpringApplication.run(CarRentalApiApplication.class, args);
+		Environment env = context.getEnvironment();
+
+		String[] activeProfiles = env.getActiveProfiles();
+		if (activeProfiles.length == 0) {
+			logger.info("No active profiles set, using default profiles.");
+		} else {
+			if (logger.isInfoEnabled()) {
+			        logger.info("Active profiles: {}", String.join(", ", activeProfiles));
+			    }
+		}
 	}
 
 }
