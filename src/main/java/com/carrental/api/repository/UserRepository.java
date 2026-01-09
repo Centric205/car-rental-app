438ca649c4ac4a50d0f01876b1ab6900e94f8ba9
Theo Madikgetla <theothedeveloper@gmail.com>
2026-01-10 01:57:30 +0200

    Add the User Repository Interface

---
+++ src/main/java/com/carrental/api/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.carrental.api.repository;
+
+import com.carrental.api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
