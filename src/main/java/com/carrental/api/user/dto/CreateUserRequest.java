65a36ae2bbee4f530b257a31364c29cd729b623a
Theo Madikgetla <theothedeveloper@gmail.com>
2026-01-16 18:36:58 +0200

    BCRA-85_Add CreateUserRequest code

---
+++ src/main/java/com/carrental/api/user/dto/CreateUserRequest.java
@@ -0,0 +1,50 @@
+package com.carrental.api.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class CreateUserRequest {
+
+    @NotBlank(message = "First name is required")
+    private String fullName;
+
+    @NotBlank
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+
+    public CreateUserRequest() {}
+
+    public CreateUserRequest(String fullName, String email, String password) {
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
